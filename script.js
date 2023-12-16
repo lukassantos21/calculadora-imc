21c1789c32e039c0a2ba568e8e164c5ac6804110
lukassantos21 <lukassousa.r.j@gmail.com>
2023-12-16 01:52:31 -0300

    validaçao do formulário

--- script.js
+++ script.js
@@ -1,56 +1,70 @@
 const btn = document.querySelector("#enviar")
 
-btn.addEventListener("click", function (e) {
-    e.preventDefault();
+btn.addEventListener("click", (event) => {
+        event.preventDefault();
 
-    const name = document.querySelector("#nome")
-    const peso = document.querySelector("#peso")
-    const altura = document.querySelector('#altura')
-    const visor = document.querySelector("#visor")
+        const name = document.querySelector("#nome");
+        const peso = document.querySelector("#peso");
+        const altura = document.querySelector('#altura');
+        const visor = document.querySelector("#visor");
+        const msgError = document.querySelector(".error")
 
 
-    const valueName = name.value
-    const valuePeso = peso.value
-    const valueAltura = altura.value
+        const valueName = name.value;
+        const valuePeso = peso.value;
+        const valueAltura = altura.value;
 
-    const alturaFinal = altura.value / 100
-    const pesoFinal = Number(peso.value)
+        if(valueName === "" || valuePeso === "" || valueAltura === ""){
+            const style = msgError.style.padding = "1rem 1rem";
+            const msgForm = msgError.textContent;            
+            msgError.textContent = "Preencha todos os campos do formulário por favor!";
+            
+            const window = visor.textContent;
+            visor.textContent = ""
 
-    const result = pesoFinal / (alturaFinal * alturaFinal)
-    const resultado = result.toFixed(2)
-  
-    let classificacao = ""
 
-    if (result < 16) {
-        classificacao = 'Baixo peso muito grave';
+            return
+        }
 
-    } else if (result >= 16 && result <= 16.99) {
-        classificacao = 'Baixo peso, grave';
+        const alturaFinal = altura.value / 100;
+        const pesoFinal = Number(peso.value);
 
-    } else if (result >= 17 && result <= 18.49) {
-        classificacao = 'Baixo peso';
+        const result = pesoFinal / (alturaFinal * alturaFinal);
+        const resultado = result.toFixed(2);
 
-    } else if (result >= 18.50 && result <= 24.99) {
-        classificacao = 'Peso normal';
+        let classificacao = "";
 
-    } else if (result >= 25 && result <= 29.99) {
-        classificacao = 'Sobrepeso';
+        if (result < 16) {
+            classificacao = 'Baixo peso muito grave';
 
-    } else if (result >= 30 && result <= 34.99) {
-        classificacao = 'Obesidade grau I';
+        } else if (result >= 16 && result <= 16.99) {
+            classificacao = 'Baixo peso, grave';
 
-    } else if (result >= 35 && result <= 39.99) {
-        classificacao = 'Obesidade grau II';
+        } else if (result >= 17 && result <= 18.49) {
+            classificacao = 'Baixo peso';
 
-    } else {
-        classificacao = 'Obesidade grau III'
-    }
+        } else if (result >= 18.50 && result <= 24.99) {
+            classificacao = 'Peso normal';
 
-    
-    const window = visor.textContent
-    visor.textContent = `Olá ${valueName}, o resultado do seu imc é: ${resultado}. Classificação: ${classificacao}`
+        } else if (result >= 25 && result <= 29.99) {
+            classificacao = 'Sobrepeso';
 
-    name.value = ""
-    altura.value = ""
-    peso.value = ""
-})
+        } else if (result >= 30 && result <= 34.99) {
+            classificacao = 'Obesidade grau I';
+
+        } else if (result >= 35 && result <= 39.99) {
+            classificacao = 'Obesidade grau II';
+
+        } else {
+            classificacao = 'Obesidade grau III';
+        }
+
+        const visorPadding = visor.style.padding = "1rem 4%s";
+        const window = visor.textContent;
+        visor.textContent = `Olá ${valueName}, o resultado do seu imc é: ${resultado}, Classificação: ${classificacao}`;
+
+        name.value = "";
+        altura.value = "";
+        peso.value = "";
+
+    })
